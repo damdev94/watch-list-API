89961385ad5f839709f4110810ae42f0ce7f443e
damien duport percier <damienviel69@gmail.com>
2024-05-28 13:26:28 -0400

    macrche

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const path = require('path')
 require('dotenv').config()
 const cors = require('cors')
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI,{ useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB!"))
   .catch(err => console.error("Error connecting to MongoDB:", err))
 
